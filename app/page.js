99bc6bce0d6c23ae6e734b0086e641f86513a16e
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-05-21 15:20:30 +0800

    added navbar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import HomeBanner from "@/components/HomeBanner";
+
 export default function Home() {
   return (
-    <div className="mt-52">
-      <h1 className="text-xl font-bold text-center">Diary of Faisal</h1>
+    <div>
+      <HomeBanner />
     </div>
   );
 }
---
+++ components/HomeBanner.jsx
@@ -0,0 +1,10 @@
+"use client"
+const HomeBanner = () => {
+  return (
+    <div className="min-h-screen">
+      <h1>This is home banner</h1>
+    </div>
+  )
+}
+
+export default HomeBanner
